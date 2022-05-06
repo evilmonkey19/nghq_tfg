eed305a3fed997eaa5cbd78ae56e457b81db9cca
evilmonkey19 <eperpinyapitarch@gmail.com>
2022-05-06 18:04:53 +0200

    c

--- examples/multicast-receiver.c
+++ examples/multicast-receiver.c
@@ -257,7 +257,7 @@ static int on_headers_cb (nghq_session *session, uint8_t flags,
           memmove(filename, filename+1, strlen(filename));
           filename = strsep(&filename, "/");
           strcat(filepath, filename);
-          printf("%s", filepath);
+          printf("FILEPATH: %s\n", filepath);
           filename_ok = true;
         }
       }
