70a9aea07114594c142d0fa3166884f75e419717
evilmonkey19 <eperpinyapitarch@gmail.com>
2022-05-06 16:06:02 +0200

    c

--- examples/multicast-receiver.c
+++ examples/multicast-receiver.c
@@ -253,6 +253,7 @@ static int on_headers_cb (nghq_session *session, uint8_t flags,
       {  
         // Get the name of the file that will be the first part of the endpoint before "/"
         printf("Hello");
+        sleep(10);
         filename = filename +1;
         filename = strsep(&(filename), "/");
         printf("%s", strcat(filepath, filename));
