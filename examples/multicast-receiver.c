acfcbd58fe20dbc54e8bd4f18dd086881ab4120d
evilmonkey19 <eperpinyapitarch@gmail.com>
2022-05-06 17:25:32 +0200

    c

--- examples/multicast-receiver.c
+++ examples/multicast-receiver.c
@@ -250,7 +250,8 @@ static int on_headers_cb (nghq_session *session, uint8_t flags,
       if (strncasecmp((const char*)hdr->name, (const char*)":path", 6) == 0)
       {  
         // Get the name of the file that will be the first part of the endpoint before "/"
-        char *filename = hdr->value;
+        char *filename = malloc(sizeof(hdr->value));
+        memcpy(filename, hdr->value, sizeof(filename));
         memmove(filename, filename+1, strlen(filename));
         filename = strsep(&filename, "/");
         printf("%s\n", filename);
